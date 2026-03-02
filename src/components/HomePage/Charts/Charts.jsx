651a54377c996ab09c76c7dbed4edd8f3448bfcb
sviatoslav-007 <sviatoslav-007@github.com>
2026-03-03 00:42:35 +0200

    fix: separate charts

---
+++ src/components/HomePage/Charts/Charts.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import styles from "./Charts.module.css";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  PointElement,
+  LineElement
+);
+
+const Charts = () => {
+  const barChartData = {
+    labels: ["Січ", "Лют", "Бер", "Кві", "Тра", "Чер", "Лип"],
+    datasets: [
+      {
+        data: [12, 19, 3, 5, 2, 3, 13],
+        backgroundColor: [
+          "#6EC1E4", 
+          "#378F97", 
+          "#0E4C6D", 
+          "#6EC1E4", 
+          "#378F97", 
+          "#0E4C6D", 
+          "#6EC1E4", 
+        ],
+        borderColor: [
+          "#6EC1E4", 
+          "#378F97", 
+          "#0E4C6D", 
+          "#6EC1E4", 
+          "#378F97", 
+          "#0E4C6D", 
+          "#6EC1E4", 
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const doughnutData = {
+    labels: ["В наявності", "В резерві", "Продано"],
+    datasets: [
+      {
+        data: [60, 20, 20],
+        backgroundColor: [
+          "#6EC1E4",
+          "#378F97", 
+          "#0E4C6D", 
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.chartContainer}>
+      <div className={styles.chartCard}>
+        <h3 className={styles.chartCardTitle}>Статистика Товарів</h3>
+        <Bar
+          data={barChartData}
+          options={{
+            responsive: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+            scales: {
+              x: {
+                grid: {
+                  display: false, 
+                },
+              },
+              y: {
+                grid: {
+                  display: false,
+                },
+              },
+            },
+          }}
+          height={150} 
+          width={350}  
+        />
+      </div>
+      <div className={styles.doughnutChartCard}>
+        <h3 className={styles.chartCardTitle}>Розподіл по категоріях</h3>
+        <Doughnut
+          data={doughnutData}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                position: "top",
+              },
+            },
+          }}
+          height={200}
+          width={200}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Charts;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,70 +2,17 @@ import React from 'react';
 import Header from './Header/Header';  
 import StatisticsCard from './StatisticsCard/StatisticsCard';  
 import InvoiceTable from './InvoiceTable/InvoiceTable';
+import Charts from './Charts/Charts';  
 
-import { Bar } from 'react-chartjs-2';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PointElement, LineElement } from 'chart.js';
 import styles from './HomePage.module.css';  
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement,
-  PointElement,
-  LineElement
-);
-
 const HomePage = () => {
-  const barChartData = {
-    labels: ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип'],
-    datasets: [
-      {
-        label: 'Статистика Товарів',
-        data: [12, 19, 3, 5, 2, 3, 13],
-        backgroundColor: '#007bff',
-        borderColor: '#007bff',
-        borderWidth: 1
-      }
-    ]
-  };
-
-  const doughnutData = {
-    labels: ['В наявності', 'В резерві', 'Продано'],
-    datasets: [
-      {
-        data: [60, 20, 20],
-        backgroundColor: [
-          '#0099FF',  
-          '#0066CC',  
-          '#003366'   
-        ],
-        hoverOffset: 4
-      }
-    ]
-  };
-
   return (
     <div className={styles.container}>
       <Header />
       <StatisticsCard /> 
-
       <InvoiceTable />
-
-      <div className={styles.charts}>
-        <div className={styles.chartCard}>
-          <h3>Статистика Товарів</h3>
-          <Bar data={barChartData} options={{ responsive: true, plugins: { title: { display: true, text: 'Статистика Товарів' } } }} />
-        </div>
-        <div className={styles.chartCard}>
-          <h3>Розподіл по категоріях</h3>
-          <Doughnut data={doughnutData} />
-        </div>
-      </div>
+      <Charts />
     </div>
   );
 };
