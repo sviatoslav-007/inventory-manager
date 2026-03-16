e7b14316a06100512e2ff92ce6da630383000302
sviatoslav-007 <sviatoslav-007@github.com>
2026-03-16 23:59:15 +0200

    finish header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { FaBox, FaCogs, FaSearch, FaList, FaBuilding, FaHome } from 'react-icons/fa'; 
-import { Link } from 'react-router-dom'; // імпортуємо Link
+import { FaBox, FaSearch, FaBuilding, FaHome, FaSignOutAlt } from 'react-icons/fa'; 
+import { Link, useNavigate } from 'react-router-dom'; 
 import styles from './Header.module.css'; 
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    
+    navigate('/');
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -20,7 +28,14 @@ const Header = () => {
         <Link to="/management">
           <FaBox className={`${styles.icon} ${styles.grayIcon}`} />
         </Link>
-        <FaCogs className={`${styles.icon} ${styles.grayIcon}`} /> 
+        
+        <button 
+          onClick={handleLogout} 
+          className={styles.logoutBtn}
+          title="Вийти"
+        >
+          <FaSignOutAlt className={`${styles.icon} ${styles.grayIcon}`} />
+        </button>
       </div>
     </div>
   );
